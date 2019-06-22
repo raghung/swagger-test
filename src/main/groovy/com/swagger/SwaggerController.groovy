df212a0b85e4842f783bdb859b74748342e8a3b3
raghung <raghunandan_gr@yahoo.com>
2019-06-21 22:48:02 -0700

    API methods

--- src/main/groovy/com/swagger/SwaggerController.groovy
+++ src/main/groovy/com/swagger/SwaggerController.groovy
@@ -1,11 +1,15 @@
 package com.swagger
 
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.views.View
 import io.swagger.v3.oas.annotations.Hidden
 
 import javax.inject.Inject
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Hidden
 @Controller('/api')
@@ -18,5 +22,30 @@ class SwaggerController {
     @Get
     SwaggerConfig index() {
         return swaggerConfig
+    }
+
+    @View("swagger/index")
+    @Get("/{url}")
+    public SwaggerConfig renderSpec(@NotNull String url) {
+        SwaggerConfig config = new SwaggerConfig()
+        config.deepLinking = swaggerConfig.deepLinking
+        config.layout = swaggerConfig.layout
+        config.version = swaggerConfig.version
+        config.urls = singletonList(new SwaggerConfig.URIConfig(name: url, url: url))
+
+        return config
+    }
+
+    @View("swagger/index")
+    @Post
+    public SwaggerConfig renderSpecs(@Body @NotEmpty List<SwaggerConfig.URIConfig> urls) {
+
+        SwaggerConfig config = new SwaggerConfig()
+        config.deepLinking = swaggerConfig.deepLinking
+        config.layout = swaggerConfig.layout
+        config.version = swaggerConfig.version
+        config.urls = urls
+
+        return config
     }
 }
